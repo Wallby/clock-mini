87f2ee872f6d821f127bd92d681d5581c0e6e4d6
maarten-laptop <maartentenvelden@hotmail.com>
2024-08-14 15:06:08 +0200

    + added test 2 which runs a timer and prompts the user to enter y/n whether the speed looked correct * test 1 now runs 5 times (5seconds, same duration as test 2) instead of 10

--- clock-mini/test.c
+++ clock-mini/test.c
@@ -17,6 +17,23 @@
 #define EPSILON_IN_MILLISECONDS 20.0
 
 
+void clear_line(FILE* a, int lineLength)
+{
+	fputs("\r", a);
+	for(int i = 0; i < lineLength; ++i)
+	{
+		fputs(" ", a); //< clear line to spaces
+	}
+}
+void flush_line(FILE* a)
+{
+	int b = fgetc(a);
+	while((b != '\n') & (b != EOF))
+	{
+		b = fgetc(a);
+	}
+}
+
 int test_1()
 {
 	double a = cm_get_milliseconds();
@@ -40,9 +57,95 @@ int test_1()
 	return 1;
 }
 
+int test_2()
+{
+	int timerDurationInSeconds = 5;
+
+	// TODO: maybe use VT100 instead of clearing line by remembering cursor position?
+	/*
+#ifdef _WIN32
+	// https://learn.microsoft.com/en-us/windows/console/clearing-the-screen#example-1
+	// https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
+	struct
+	{
+		HANDLE a;
+	} stdoutputhandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	DWORD consolemode;
+	if(GetConsoleMode(stdoutputhandle.a, &consolemode) == FALSE)
+	{
+		int a;
+		getlasterror_to_string(&a, 0);
+		char b[a];
+		getlasterror_to_string(&a, b);
+
+		fprintf(stderr, "error: %s\n", b);
+		return 0;
+	}
+
+	DWORD newConsolemode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+	if(SetConsoleMode(stdoutputhandle.a, &newConsolemode) == FALSE)
+	{
+		int a;
+		getlasterror_to_string(&a, 0);
+		char b[a];
+		getlasterror_to_string(&a, b);
+
+		fprintf(stderr, "error: %s\n", b);
+		return 0;
+	}
+
+	// and below..
+	//if(SetConsoleMode(stdoutputhandle.a, consolemode) == FALSE)
+	//{
+	//  //...
+	//}
+#endif
+	*/
+
+	printf("Starting timer for %i seconds\n", timerDurationInSeconds);
+	double a = cm_get_seconds();
+	double b = a;
+	int stdoutLineLength = 0;
+	while(1)
+	{
+		double deltatime = b - a;
+		if((int)deltatime >= timerDurationInSeconds)
+		{
+			break;
+		}
+
+		clear_line(stdout, stdoutLineLength);
+		stdoutLineLength = printf("Time %f", deltatime);
+
+		b = cm_get_seconds();
+	}
+	clear_line(stdout, stdoutLineLength);
+	printf("Time %f\n", (float)timerDurationInSeconds);
+
+	fputs("Did the time advance at a correct-looking speed? y/n: ", stdout);
+	while(1)
+	{
+		char a = fgetc(stdin); //< will add \n automatically
+		flush_line(stdin);
+		if(a == 'n')
+		{
+			return 0;
+		}
+		if(a == 'y')
+		{
+			break;
+		}
+		fputs("Answer not recognised, try again. y/n: ", stdout);
+	}
+
+	return 1;
+}
+
 int main(int argc, char** argv)
 {
-	TM_TEST(1, 9);
+	TM_TEST(1, 4);
+	TM_TEST2(2);
 	
 	return 0;
 }
